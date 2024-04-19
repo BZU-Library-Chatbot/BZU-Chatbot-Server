1a6cf173b2691ce6a4e1a910e9b9bb737b0cb9dc
RoOtT24 <1190579@student.birzeit.edu>
2024-04-19 12:53:15 +0200

    adding asyncHandler to refreshToken

--- src/Modules/Auth/Auth.router.js
+++ src/Modules/Auth/Auth.router.js
@@ -44,7 +44,7 @@ router.patch(
 router.post(
   "/refresh",
   validation(validators.refresh),
-  (AuthController.refreshToken)
+  asyncHandler(AuthController.refreshToken)
 );
 
 export default router;
