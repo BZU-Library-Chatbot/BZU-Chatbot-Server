b97f9c7958d360602ded583e5c6d2b8aa46264e1
Aziza Karakra <122555451+azizakarakra@users.noreply.github.com>
2024-05-03 22:25:28 +0300

    Update auth.test.ts

--- src/tests/Auth/auth.test.ts
+++ src/tests/Auth/auth.test.ts
@@ -4,37 +4,6 @@ import app from "../../..";
 
 dotenv.config();
 
-// describe("POST /auth/signup", () => {
-//   it.each([
-//     [400, "Aziza", "azizagmail.com", "password123", "password123"], // Invalid email format
-//     [201, "Aziza", "azizakarakra7@gmail.com", "password123", "password123"], // Valid input
-//     [
-//       400,
-//       "Existing User",
-//       "azizakarakra7@gmail.com",
-//       "existingpassword",
-//       "existingpassword",
-//     ], // Email already exists
-//     [400, "User", "user@example.com", "password123", "password456"], // Passwords don't match
-//     [400, "User", "user@example.com", "abc", "abc"], // Password too short
-//     [400, "", "user@example.com", "abcabcabcabc", "abcabcabcabc"], //Empty username
-//     [400, "User", "", "password123", "password123"], //Empty email
-//     [400, "User", "user@example.com", "", "password456"], //Empty password
-//     [400, "User", "user@example.com", "password123", ""], //Empty Cpassword
-//     [400, "User", "user@example.com", "", ""], //Empty password & empty Cpassword
-//     [400, "", "", "", ""], //Empty fileds
-//     [400, "", "", "password123", "password123"], //Empty username & email
-//   ])(
-//     "should return %i when given %s, %s, %s, %s",
-//     async (expected, userName, email, password, cPassword) => {
-//       const res = await request(app)
-//         .post("/auth/signup")
-//         .send({ userName, email, password, cPassword });
-//       expect(res.statusCode).toBe(expected);
-//     }
-//   );
-// });
-
 describe("POST /auth/login", () => {
   let variables: any = {};
   it.each([
@@ -104,17 +73,3 @@ describe("POST /auth/login", () => {
     expect(res.statusCode).toBe(expected);
   });
 });
-
-// describe("POST /auth/refreshToken", () => {
-//   it.each([
-//     [400, ""], // Empty refreshToken
-//     [
-//       400,
-//       "Aziza__eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2MjZhNGNhMGU5NTdlMjY4ggggggggggggggggggggggggggoxNzEzODEzOTY2fQ.pvEPu437RLxr5euEit3RBRTX4bQZ46OvAceJKj0b1Bo",
-//     ], // Invalid refreshToken format
-//     [200, variables.refreshToken], // Valid refreshToken
-//   ])("should return %i when given %s", async (expected, refreshToken) => {
-//     const res = await request(app).post("/auth/refresh").send({ refreshToken }); // sending refreshToken in the request body
-//     expect(res.statusCode).toBe(expected);
-//   });
-// });
