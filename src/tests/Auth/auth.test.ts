09b240d8f6de5ae0d31287e06ab798ffd4fff4b9
Aziza Karakra <122555451+azizakarakra@users.noreply.github.com>
2024-05-03 18:59:18 +0300

    Update auth.test.ts

--- src/tests/Auth/auth.test.ts
+++ src/tests/Auth/auth.test.ts
@@ -20,9 +20,9 @@ describe("POST /auth/signup", () => {
     [400, "", "user@example.com", "abcabcabcabc", "abcabcabcabc"], //Empty username
     [400, "User", "", "password123", "password123"], //Empty email
     [400, "User", "user@example.com", "", "password456"], //Empty password
-    [400, "User", "user@example.com", "password123", ""],//Empty Cpassword
+    [400, "User", "user@example.com", "password123", ""], //Empty Cpassword
     [400, "User", "user@example.com", "", ""], //Empty password & empty Cpassword
-    [400, "", "", "", ""], //Empty fileds 
+    [400, "", "", "", ""], //Empty fileds
     [400, "", "", "password123", "password123"], //Empty username & email
   ])(
     "should return %i when given %s, %s, %s, %s",
