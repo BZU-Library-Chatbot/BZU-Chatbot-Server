83b732b59b88a4b39205b585ccfc0475ce40e7c5
Aziza Karakra <122555451+azizakarakra@users.noreply.github.com>
2024-05-03 18:27:10 +0300

    updated signup test

--- src/tests/Auth/auth.test.ts
+++ src/tests/Auth/auth.test.ts
@@ -17,6 +17,13 @@ describe("POST /auth/signup", () => {
     ], // Email already exists
     [400, "User", "user@example.com", "password123", "password456"], // Passwords don't match
     [400, "User", "user@example.com", "abc", "abc"], // Password too short
+    [400, "", "user@example.com", "abcabcabcabc", "abcabcabcabc"], //Empty username
+    [400, "User", "", "password123", "password123"], //Empty email
+    [400, "User", "user@example.com", "", "password456"], //Empty password
+    [400, "User", "user@example.com", "password123", ""],//Empty Cpassword
+    [400, "User", "user@example.com", "", ""], //Empty password & empty Cpassword
+    [400, "", "", "", ""], //Empty fileds 
+    [400, "", "", "password123", "password123"], //Empty username & email
   ])(
     "should return %i when given %s, %s, %s, %s",
     async (expected, userName, email, password, cPassword) => {
