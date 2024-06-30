f04fcc755651b9717efac622d1fc25163eede2ff
Aziza Karakra <122555451+azizakarakra@users.noreply.github.com>
2024-06-30 17:09:27 +0300

    getFeedbackById endpoint and integration test

--- src/Modules/Feedback/controller/Feedback.controller.ts
+++ src/Modules/Feedback/controller/Feedback.controller.ts
@@ -74,3 +74,23 @@ export const deleteFeedback = async (req: any, res: any, next: any) => {
 
   return res.status(200).json({ message: "Feedback deleted successfully" });
 };
+
+export const getFeedbackById = async (req: any, res: any, next: any) => {
+  const { feedbackId } = req.params;
+  const feedback = await feedbackModel
+    .findById(feedbackId)
+    .populate({
+      path: "interactionId",
+      populate: {
+        path: "userId",
+        model: "User",
+      },
+    });
+  //const userId = feedback.interactionId.userId._id;
+  return res.status(200).json({
+    message: "success",
+    feedback,
+    
+  });
+
+}
